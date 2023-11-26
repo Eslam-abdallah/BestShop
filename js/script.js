6c9d48f264bedd8a2962f39005b5d541e0c0b3be
Eslam Abdallah Mohamed Elkomy <124469180+Eslam-abdallah@users.noreply.github.com>
2023-11-26 20:06:50 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-var togle =document.querySelector(".navbar-toggler");
-var icon =document.querySelector(".navbar-toggler svg");
-var close =`<h1>Solom</h1>`;
-togle.addEventListener("click" , function(){
-    icon.classList.toggle("fa-x");
-});
+
